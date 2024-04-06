dad468f3841e4f6e6702661107fcd1543dc0667c
IshanLahiru <ishanlahiru2002@gmail.com>
2024-04-06 22:28:15 +0530

    Update Jest configuration

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -1,7 +1,11 @@
-module.exports = {
+import type { Config } from 'jest';
+
+const config: Config = {
   verbose: true,
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./test/setup-jest-env.js'],
   testMatch: ['**/test/**/*.test.[jt]s?(x)'],
   transformIgnorePatterns: [`/node_modules/(?!nanoid)`],
 };
+
+export default config;
