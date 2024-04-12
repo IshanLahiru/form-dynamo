3e4de6aef4ebed7209b42e3ba1857268a9f6b02f
IshanLahiru <ishanlahiru2002@gmail.com>
2024-04-12 22:19:32 +0530

    Add fields module for handling form fields in components

---
+++ packages/core/src/components/fields/index.ts
@@ -0,0 +1,13 @@
+import StringField from './StringField';
+import DateTimeField from './dateTimeField';
+import NumberField from './NumberField';
+
+function fields() {
+  return {
+    StringField,
+    DateTimeField,
+    NumberField,
+  };
+}
+
+export default fields;
