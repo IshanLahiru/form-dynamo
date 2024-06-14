0a722d41c620dde2eab19a2fc01b698547a85777
IshanLahiru <ishanlahiru2002@gmail.com>
2024-06-14 12:18:09 +0530

    feat: Add DateTimeField component for handling date and time inputs

---
+++ packages/antd/src/components/fields/BaseField.tsx
@@ -0,0 +1,31 @@
+import { DatePicker, Form } from 'antd';
+import moment from 'moment';
+
+interface DateTimeFieldProps {
+  id: string;
+  name: string;
+  title: string;
+  formData: string;
+  onChange: (value: string) => void;
+  error: string;
+}
+
+export default function DateTimeField(props: DateTimeFieldProps) {
+  return (
+    <Form.Item
+      label={props.title}
+      validateStatus={props.error ? 'error' : ''}
+      help={props.error}
+    >
+      <DatePicker
+        id={props.id}
+        name={props.name}
+        value={props.formData ? moment(props.formData) : null}
+        onChange={(value) =>
+          props.onChange(value ? value.format('YYYY-MM-DDTHH:mm') : '')
+        }
+        showTime
+      />
+    </Form.Item>
+  );
+}
