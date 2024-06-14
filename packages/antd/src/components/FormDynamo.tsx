ebbf518d7d6735c27dad608ab926589448174302
IshanLahiru <ishanlahiru2002@gmail.com>
2024-06-14 12:18:39 +0530

    feat: Add FormDynamo component for dynamic form rendering

---
+++ packages/antd/src/components/FormDynamo.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+export interface FormProps {
+  Schema?: Record<string, unknown>;
+  formData?: Record<string, unknown>;
+  readonly?: boolean;
+}
+
+export default function Form(props: FormProps) {
+  const [fieldArray, setFieldArray] = useState<Record<string, unknown>[]>([]);
+  useEffect(() => {
+    if (props.Schema) {
+      console.log('the schema is: ', props.Schema);
+      //schemaConverter(props.Schema);
+    }
+  }, [props]);
+
+  return (
+    <div className="container">
+      {fieldArray.map((field) => {
+        return <div />;
+      })}
+    </div>
+  );
+}
---
+++ packages/antd/src/index.ts
@@ -0,0 +1,4 @@
+import Form, { FormProps } from './components/FormDynamo';
+
+export type { FormProps };
+export default Form;
